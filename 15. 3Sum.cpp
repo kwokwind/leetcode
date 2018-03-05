ef2bf63a92037512ce9bc0e0b8fdef969d2d2bc1
kwokwind <kwokwind.us@gmail.com>
2018-03-04 22:37:40 -0600

    Update 15. 3Sum.cpp

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -8,10 +8,6 @@ public:
         const int target = 0;
         sort(nums.begin(), nums.end());
         
-        for (auto a : nums) {
-            cout << a << ",";
-        }
-        
         auto last = nums.end();
         for(auto i=nums.begin(); i<last-2; ++i) {
             if (i > nums.begin() && *i == *(i-1)) continue;
@@ -24,7 +20,7 @@ public:
                     while (j < k && *j == *(j-1)) ++j;
                 } else if ((*i + *j + *k) > target) {
                     --k;
-                    while (k > j && *k == *(k+1)) --k;
+                    while (j < k && *k == *(k+1)) --k;
                 } else {
                     ret.push_back({*i,*j,*k});
                     ++j;
