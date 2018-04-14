3727525b9b1c798362419943d2a90a2fa3589215
kwokwind <kwokwind.us@gmail.com>
2018-04-14 10:09:39 -0500

    Update 134. Gas Station.cpp

--- 134. Gas Station.cpp
+++ 134. Gas Station.cpp
@@ -7,6 +7,31 @@ Note:
 The solution is guaranteed to be unique.
 */
 
+//method 02
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int j = -1;
+        int sum = 0;
+        int total = 0;
+        for (int i=0; i<gas.size(); ++i) {
+            sum += gas[i] - cost[i];
+            total += gas[i] - cost[i];
+            if (sum < 0) {
+                // this gas station is not valid, start from next station,
+                // and reset sum to 0
+                j = i;
+                sum = 0;
+            }
+        }
+        if (total >= 0)
+            return j+1;
+        else
+            return -1;
+    }
+};
+
+//method 01
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
